b4e77cb372bce99211b14e5114b389b1e790e2a3
Sam cook <samuel.cook29@student.tamdistrict.org>
2026-03-19 22:27:32 +0000

    js work

---
+++ script.js
@@ -0,0 +1,9 @@
+let glasses = document.getElementById("glasses");
+let cat = document.getElementById("cat");
+let score = 0;
+function cats() {
+    score += 1;
+    console.log("check");
+}
+cat.addEventListener(onclick, cats);
+console.log("check");
